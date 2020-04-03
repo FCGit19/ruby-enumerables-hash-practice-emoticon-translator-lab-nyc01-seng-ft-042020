01edd38391308a7d186b3fee87ad321604cdac5f
Fred C <canofred@gmail.com>
2020-04-03 20:12:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ end
 def get_english_meaning(path, symbol)
   each_emoticon = load_library(path)
     each_emoticon.each do |name|
-      if each_emoticon[name]
+      if each_emoticon[]
         binding.pry
       end
     end
