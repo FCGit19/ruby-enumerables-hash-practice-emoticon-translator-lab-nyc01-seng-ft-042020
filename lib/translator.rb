90dd4b9e8a7bbacdfcc0e8704a19655ba15ebf21
Fred C <canofred@gmail.com>
2020-04-03 19:00:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,10 +18,13 @@ def load_library(path)
 end
 
 
-def get_japanese_emoticon
-  # code goes here
-end
+# def get_japanese_emoticon
+#   # code goes here
+# end
 
-def get_english_meaning
-  # code goes here
+
+
+def get_english_meaning(load_library, symbol)
+  
+  
 end
