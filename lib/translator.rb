22da0bc3505af16686158d1869c24e9e6c3a2d66
Fred C <canofred@gmail.com>
2020-04-03 18:55:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,13 +7,10 @@ def load_library(path)
   emoticon_names = {}
   
     emoticons.each do |name, symbol|
-      # binding.pry
+   
       if !emoticon_names[name]
         emoticon_names[name] = {:english => symbol[0], :japanese => symbol[1]}
-        # binding.pry
       end 
-        
-        
       
     end
    
