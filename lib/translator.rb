de4437432bd2b5204d5499ac844c38162e1a92c2
Fred C <canofred@gmail.com>
2020-04-03 22:24:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -35,7 +35,7 @@ def get_english_meaning(path, symbol)
       end 
         # binding.pry
         
-        if each_emoticon[name][:japanese] != symbol
+        if each_emoticon[name][:japanese] == nil
         end
         translation = "Sorry, that emoticon was not found"
         
