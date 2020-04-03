95260bf2cf61712d4ba6d3f0890ce9803b6b952b
Fred C <canofred@gmail.com>
2020-04-03 21:41:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,8 +28,8 @@ end
 def get_english_meaning(path, symbol)
   each_emoticon = load_library(path)
     each_emoticon.each do |name, value|
-      if each_emoticon[name][:japanese] 
-        each_emoticon[name][:english]
+      if each_emoticon[name][:japanese] == symbol
+        each_emoticon[name]
         binding.pry
       
     end
