c7c9e71e2bb9746c39921745f63af7d5f6ff5836
Fred C <canofred@gmail.com>
2020-04-03 21:03:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,13 @@ require 'yaml'
 def load_library(path)
   emoticons = YAML.load_file(path)
   
-  emoticon_names = {}
+  # emoticon_names = {}
   
-    emoticons.each do |name, symbol|
+  #   emoticons.each do |name, symbol|
    
-      if !emoticon_names[name]
-        emoticon_names[name] = {:english => symbol[0], :japanese => symbol[1]}
-      end 
+  #     if !emoticon_names[name]
+  #       emoticon_names[name] = {:english => symbol[0], :japanese => symbol[1]}
+  #     end 
       
     end
    
