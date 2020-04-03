88b0e6abb3c51137ccdc11dcc888ee7ab588b34f
Fred C <canofred@gmail.com>
2020-04-03 23:19:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(path, symbol)
     each_emoticon.each do |name, value|
       
       if each_emoticon[name][:english] == symbol
-        return name 
+        return symbol 
       end 
       
     end
